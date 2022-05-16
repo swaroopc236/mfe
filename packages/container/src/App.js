18a9334f319b2b6350ef97ca40888f2bf26e6105
swaroopc236 <swaroopc236@gmail.com>
2022-05-16 19:38:38 +0530

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+	return (
+		<div>
+			<h1>Container</h1>
+			<hr></hr>
+			<MarketingApp />
+		</div>
+	);
+};
